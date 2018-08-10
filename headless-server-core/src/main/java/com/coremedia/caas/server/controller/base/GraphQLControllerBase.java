a0e06136ea15e2c8d8b0ede3002a269189eee3f6
agawecki <andreas.gaweckicoremedia.com>
2018-08-10 10:27:58 +0200

    dependency tracked grahql queries, conditional request support w/ etags

--- headless-server-core/src/main/java/com/coremedia/caas/server/controller/base/GraphQLControllerBase.java
+++ headless-server-core/src/main/java/com/coremedia/caas/server/controller/base/GraphQLControllerBase.java
@@ -10,22 +10,41 @@ import com.coremedia.caas.service.ServiceRegistry;
 import com.coremedia.caas.service.repository.RootContext;
 import com.coremedia.caas.service.security.AccessControlViolation;
 import com.coremedia.cache.Cache;
-
+import com.coremedia.cache.CacheKey;
+import com.coremedia.cap.multisite.Site;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Objects;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.GraphQLError;
+import graphql.execution.instrumentation.InstrumentationContext;
+import graphql.execution.instrumentation.InstrumentationState;
+import graphql.execution.instrumentation.SimpleInstrumentation;
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
+import graphql.execution.instrumentation.parameters.InstrumentationFieldFetchParameters;
+import graphql.schema.DataFetcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.ServletWebRequest;
 
-import java.util.Map;
-import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static graphql.execution.instrumentation.SimpleInstrumentationContext.whenCompleted;
 
 public abstract class GraphQLControllerBase extends ControllerBase {
 
@@ -41,6 +60,7 @@ public abstract class GraphQLControllerBase extends ControllerBase {
   @Autowired
   private ServiceRegistry serviceRegistry;
 
+  @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
   @Autowired
   private SettingsService settingsService;
 
@@ -64,8 +84,22 @@ public abstract class GraphQLControllerBase extends ControllerBase {
             .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue()[0]));
   }
 
+  private Object runQuery(@NotNull RootContext rootContext, @NotNull ClientIdentification clientIdentification, @NotNull String queryName, @NotNull String viewName, Map<String, Object> queryArgs) {
+    boolean doCache = true;
+    if (!doCache) {
+      return evalQuery(rootContext, clientIdentification, queryName, viewName, queryArgs);
+    }
+    JsonResult jsonResult = cache.get(new GraphQLQueryCacheKey(rootContext, clientIdentification, queryName, viewName, queryArgs));
 
-  private Object runQuery(@NotNull RootContext rootContext, @NotNull ClientIdentification clientIdentification, @NotNull String queryName, @NotNull String viewName, Map<String, Object> queryArgs, ServletWebRequest request) {
+    return ResponseEntity
+            .ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .eTag(jsonResult.eTag)
+            //.lastModified(jsonResult.timestamp.toEpochMilli()) inaccurate fallback, etag is superior
+            .body(jsonResult.json);
+  }
+
+  private Object evalQuery(@NotNull RootContext rootContext, @NotNull ClientIdentification clientIdentification, @NotNull String queryName, @NotNull String viewName, Map<String, Object> queryArgs) {
     String definitionName = clientIdentification.getDefinitionName();
     // repository defined runtime definition
     ProcessingDefinitionCacheKey processingDefinitionCacheKey = new ProcessingDefinitionCacheKey(rootContext.getSite().getSiteIndicator(), settingsService, applicationContext);
@@ -90,6 +124,7 @@ public abstract class GraphQLControllerBase extends ControllerBase {
             .build();
     ExecutionResult result = GraphQL.newGraphQL(processingDefinition.getQuerySchema(rootContext.getTarget(), queryName, viewName))
             .preparsedDocumentProvider(processingDefinition.getQueryRegistry())
+            .instrumentation(new DependencyTrackingInstrumentation())
             .build()
             .execute(executionInput);
     if (!result.getErrors().isEmpty()) {
@@ -97,17 +132,16 @@ public abstract class GraphQLControllerBase extends ControllerBase {
         LOG.error("GraphQL execution error: {}", error.toString());
       }
     }
+    result.toSpecification();
     return result.getData();
   }
-
 
   protected Object execute(String tenantId, String siteId, String queryName, String targetId, String viewName, ServletWebRequest request) {
     try {
       RootContext rootContext;
       if (targetId == null) {
         rootContext = resolveRootContext(tenantId, siteId, request);
-      }
-      else {
+      } else {
         rootContext = resolveRootContext(tenantId, siteId, targetId, request);
       }
       // determine client
@@ -119,7 +153,7 @@ public abstract class GraphQLControllerBase extends ControllerBase {
       // initialize expression evaluator
       serviceRegistry.getExpressionEvaluator().init(queryArgs);
       // run query
-      return execute(() -> runQuery(rootContext, clientIdentification, queryName, viewName, queryArgs, request), "tenant", tenantId, "site", siteId, "client", clientId, "pd", definitionName, "query", queryName, "view", viewName);
+      return execute(() -> runQuery(rootContext, clientIdentification, queryName, viewName, queryArgs), "tenant", tenantId, "site", siteId, "client", clientId, "pd", definitionName, "query", queryName, "view", viewName);
     } catch (AccessControlViolation e) {
       return handleError(e, request);
     } catch (ResponseStatusException e) {
@@ -128,4 +162,131 @@ public abstract class GraphQLControllerBase extends ControllerBase {
       return handleError(e, request);
     }
   }
+
+
+  class DependencyTrackingInstrumentationState implements InstrumentationState {
+    boolean contextSet = false;
+    Cache.Context cacheContext = Cache.currentContext();
+  }
+
+  class DependencyTrackingInstrumentation extends SimpleInstrumentation {
+    @Override
+    public InstrumentationState createState() {
+      //
+      // instrumentation state is passed during each invocation of an Instrumentation method
+      // and allows you to put stateful data away and reference it during the query execution
+      //
+      return new DependencyTrackingInstrumentationState();
+    }
+
+    @Override
+    public InstrumentationContext<ExecutionResult> beginExecution(InstrumentationExecutionParameters parameters) {
+      return whenCompleted((result, t) -> {
+        DependencyTrackingInstrumentationState state = parameters.getInstrumentationState();
+        Cache.Context context = Cache.currentContext();
+        if (context != state.cacheContext) {
+          // will throw IllegalStateException if current context is non-null
+          Cache.setContext(state.cacheContext);
+          state.contextSet = true;
+        }
+      });
+    }
+
+    @Override
+    public DataFetcher<?> instrumentDataFetcher(DataFetcher<?> dataFetcher, InstrumentationFieldFetchParameters parameters) {
+      // maybe we haveto check whether dependency tracker is set up for the current thread
+      //DependencyTrackingInstrumentationState customInstrumentationState = parameters.getInstrumentationState();
+      return super.instrumentDataFetcher(dataFetcher, parameters);
+    }
+
+    @Override
+    public CompletableFuture<ExecutionResult> instrumentExecutionResult(ExecutionResult executionResult, InstrumentationExecutionParameters parameters) {
+      DependencyTrackingInstrumentationState dependencyTrackingInstrumentationState = parameters.getInstrumentationState();
+      if (dependencyTrackingInstrumentationState.contextSet) {
+        Cache.unsetContext();
+      }
+      return CompletableFuture.completedFuture(executionResult);
+    }
+
+  }
+
+  static class JsonResult {
+    String json;
+    String eTag;
+    Instant timestamp;
+
+    JsonResult(String json, String eTag) {
+      this.json = json;
+      this.eTag = eTag;
+      this.timestamp = Instant.now();
+    }
+  }
+
+  class GraphQLQueryCacheKey extends CacheKey<JsonResult> {
+
+
+    @NotNull
+    private RootContext rootContext;
+
+    @NotNull
+    private ClientIdentification clientIdentification;
+
+    @NotNull
+    private String queryName;
+
+    @NotNull
+    private String viewName;
+
+    private Map<String, Object> queryArgs;
+
+    GraphQLQueryCacheKey(RootContext rootContext, ClientIdentification clientIdentification, String queryName, String viewName, Map<String, Object> queryArgs) {
+      this.rootContext = rootContext;
+      this.clientIdentification = clientIdentification;
+      this.queryName = queryName;
+      this.viewName = viewName;
+      this.queryArgs = queryArgs;
+    }
+
+    @Override
+    public JsonResult evaluate(Cache cache) throws JsonProcessingException, NoSuchAlgorithmException {
+      Object o = evalQuery(rootContext, clientIdentification, queryName, viewName, queryArgs);
+      ObjectMapper objectMapper = new ObjectMapper();
+      String json = objectMapper.writeValueAsString(o);
+      objectMapper.writeValueAsString(o);
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      md.update(json.getBytes());
+      byte[] digest = md.digest();
+      String etag = DatatypeConverter
+              .printHexBinary(digest).toUpperCase();
+      return new JsonResult(json, etag);
+    }
+
+    @Override
+    public int hashCode() {
+      Site site = rootContext.getSite();
+      Object  target = rootContext.getTarget();
+
+      int hashCode = Objects.hashCode(site, target, clientIdentification, queryName, viewName, queryArgs);
+      return hashCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      GraphQLQueryCacheKey dck = (GraphQLQueryCacheKey) o;
+      boolean b = queryName.equals(dck.queryName) &&
+              viewName.equals(dck.viewName) &&
+              clientIdentification.equals(dck.clientIdentification) &&
+              Objects.equal(queryArgs, dck.queryArgs) &&
+              Objects.equal(rootContext.getSite(), dck.rootContext.getSite());
+      return b;
+    }
+  }
+
+
 }
--- headless-server-core/src/main/java/com/coremedia/caas/server/service/security/ValidityDateValidator.java
+++ headless-server-core/src/main/java/com/coremedia/caas/server/service/security/ValidityDateValidator.java
@@ -4,8 +4,10 @@ import com.coremedia.caas.service.repository.RootContext;
 import com.coremedia.caas.service.security.AccessControlResultCode;
 import com.coremedia.caas.service.security.AccessValidator;
 import com.coremedia.cap.content.Content;
+import com.coremedia.cotopaxi.common.CacheUtil;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import static com.coremedia.caas.server.service.request.ContextProperties.REQUEST_DATE;
 
@@ -26,6 +28,13 @@ public class ValidityDateValidator implements AccessValidator<Content> {
   public boolean validate(Content target, RootContext rootContext) {
     if (target.getType().isSubtypeOf("CMLinkable")) {
       Calendar now = rootContext.getRequestContext().getProperty(REQUEST_DATE, Calendar.class);
+      if (now == null) {
+        now = new GregorianCalendar();
+      } else {
+        CacheUtil.uncacheable();
+        //TODO: upgrade to  1804 and use
+        // Cache.uncacheable();
+      }
       Calendar validFrom = target.getDate("validFrom");
       Calendar validTo = target.getDate("validTo");
       return (validFrom == null || validFrom.compareTo(now) <= 0) && (validTo == null || validTo.compareTo(now) > 0);
